5857cc141fd085c1cf49cdc93827f9c002890ae6
unknown <Aprendiz@BOGDFPCGMS4140.FormacionSena.RED>
2024-02-16 10:20:36 -0500

    Registro de E/S completos

--- src/main/java/org/ptech081/parking/Main.java
+++ src/main/java/org/ptech081/parking/Main.java
@@ -1,11 +1,19 @@
 package org.ptech081.parking;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sound.midi.Soundbank;
+
 import org.ptech081.parking.entities.*;
 public class Main {
     public static void main(String[] args) {
     //crear carros con los constructores:
-    Carro carrito1 = new Carro("DDP695",TipoVehiculo.MOTO);
-    Carro carrito2 = new Carro("ASD235",TipoVehiculo.CAMION);
+    Carro carrito1 = new Carro("DDP 695",TipoVehiculo.MOTO);
+    Carro carrito2 = new Carro("ASD 235",TipoVehiculo.CAMION);
     Carro carrito3 = new Carro();
     carrito3.placa = "JHG654";
     carrito3.tipoVehiculo = TipoVehiculo.CAMIONETA;
@@ -20,11 +28,39 @@ public class Main {
     //invocar el metodo addCar:
     cliente1.addCar(carrito1);
     cliente1.addCar("ASB 223", TipoVehiculo.BUS);
-    //Recorrer los carros del cliente
-    for( Carro c : cliente1.misCarros ){
-        System.out.println(c.placa + " "+ c.tipoVehiculo);
-        System.out.println("----------------");
+    cliente2.addCar(carrito2);
+    cliente2.addCar("AEB 223", TipoVehiculo.CAMION);
+    
+    //crear cupo
+    Cupo cupo1 = new Cupo('A'); 
+    Cupo cupo2 = new Cupo('B'); 
+    
+    //crear dos registros E/S
+    LocalDate fechaInicio = LocalDate.now();
+    LocalTime horaInicio = LocalTime.now();
+    LocalDate fechaFin = LocalDate.of(2024, Month.MARCH, 24);
+    LocalTime horaFin = LocalTime.of(10, 59, 50);
+    List<Registro> misRegistros= new ArrayList<Registro>();
+
+    LocalDate fechaInicio2 = LocalDate.now();
+    LocalTime horaInicio2 = LocalTime.now();
+    LocalDate fechaFin2 = LocalDate.of(2025, Month.MARCH, 29);
+    LocalTime horaFin2 = LocalTime.of(20, 30, 00);
+
+    //crear registro
+    Registro registro1 = new Registro(fechaInicio, horaInicio, fechaFin,horaFin, 5000L,cupo1,cliente1.misCarros.get(1),cliente1);
+    Registro registro2 = new Registro(fechaInicio2, horaInicio2, fechaFin2,horaFin2, 6000L,cupo1,cliente2.misCarros.get(1),cliente2);
+    //Añadir el registro a la lista
+    misRegistros.add(registro1);
+    misRegistros.add(registro2);
+    
+    System.out.println("registros de entreda y salida para parking");
+    
+    for(Registro r:misRegistros){
+        System.out.println("Cliente: " +r.cliente.nombre + " " +r.cliente.Apellido + "|" + r.carro.placa + " " + r.carro.tipoVehiculo + "|" + r.valor+ "|" + r.fechaInicio +" "+ r.horaInicio + "|" );
     }
+
     }
+    
 }
 
--- src/main/java/org/ptech081/parking/entities/Cupo.java
+++ src/main/java/org/ptech081/parking/entities/Cupo.java
@@ -2,4 +2,14 @@ package org.ptech081.parking.entities;
 
 public class Cupo {
 
+    public char cupo;
+
+    public Cupo() {
+    }
+
+    public Cupo(char cupo) {
+        this.cupo = cupo;
+    }
+    
+    
 }
